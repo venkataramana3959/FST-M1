c2f33262de850d8b8ace15d98839243434fd39b5
venkataramana3959 <Peddinenikalva.Venkataramana@ibm.com>
2024-07-19 14:06:51 +0530

    Add files via upload

---
+++ SQL/Activities/sqlscript (1).sql
@@ -0,0 +1,36 @@
+REM   Script: Session m1-june24 Activity1
+REM   just some practice
+
+select * from scott.emp;
+
+select * from scott.emp;
+
+select * from scott.emp;
+
+select*from scott.emp;
+
+select*from scott.emp;
+
+select * from scott.dept;
+
+select*from scott.emp;
+
+select * from scott.dept;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+describe salesman
+
+
